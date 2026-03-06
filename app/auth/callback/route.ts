53c00e13b8df7ed41fecdb09fd958fa802160cec
jolene-wang <jcw2239@columbia.edu>
2026-03-06 10:59:24 -0600

    testing superadmin

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,15 +1,12 @@
+import { createClient } from '@/lib/supabase-server'
 import { NextResponse } from 'next/server'
-import { createBrowserClient } from '@supabase/ssr'
 
 export async function GET(request: Request) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
 
   if (code) {
-    const supabase = createBrowserClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
+    const supabase = await createClient()
     await supabase.auth.exchangeCodeForSession(code)
   }
 
